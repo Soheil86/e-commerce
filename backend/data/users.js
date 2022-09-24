785f20b457b6f775968d517e6c74cd624c8f7b0d
Soheil86 <soheilghanbari1365@gmail.com>
2022-09-24 16:00:18 +0200

    creating Models and seeder to imported data into mongoDB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const user = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Soheil Ghanbari',
+    email: 'soheil@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Arezoo Mirza',
+    email: 'arezoo@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default user
---
+++ backend/models/productModel.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamp: true,
+  }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    display_image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    is_best_seller: {
+      type: Boolean,
+      required: false,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamp: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
